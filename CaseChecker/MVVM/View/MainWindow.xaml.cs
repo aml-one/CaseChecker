0164d792137004700289af54896e18420bf7f108
Ambrus L <ambrus@aml.nyc>
2024-05-16 12:52:13 +0100

    Bug Fix - still issue with taskbar beign on left of the screen

--- CaseChecker/MVVM/View/MainWindow.xaml.cs
+++ CaseChecker/MVVM/View/MainWindow.xaml.cs
@@ -7,11 +7,14 @@ using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Input;
+using static CaseChecker.MVVM.View.MainWindow.NativeMethods;
+using System.Windows.Interop;
 
 namespace CaseChecker.MVVM.View
 {
@@ -373,9 +376,23 @@ namespace CaseChecker.MVVM.View
             MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
             MaxWidth = SystemParameters.MaximizedPrimaryScreenWidth;
 
-            double ActualScreenHeight = SystemParameters.PrimaryScreenHeight;
-            double ActualScreenWidth = SystemParameters.PrimaryScreenWidth;
-                        
+            double ActualScreenHeight = MaxHeight;
+            double ActualScreenWidth = MaxWidth;
+
+            var hwnd = new WindowInteropHelper(this).EnsureHandle();
+            var monitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+
+            if (monitor != IntPtr.Zero)
+            {
+                var monitorInfo = new NativeMonitorInfo();
+                GetMonitorInfo(monitor, monitorInfo);
+
+                //var left = monitorInfo.Monitor.Left;
+                //var top = monitorInfo.Monitor.Top;
+                ActualScreenWidth = (monitorInfo.Monitor.Right - monitorInfo.Monitor.Left);
+                ActualScreenHeight = (monitorInfo.Monitor.Bottom - monitorInfo.Monitor.Top);
+            }
+
             if (WindowState == WindowState.Maximized)
             {
                 WindowState = WindowState.Normal;
@@ -401,6 +418,49 @@ namespace CaseChecker.MVVM.View
 
             }
 
+        }
+
+        public static class NativeMethods
+        {
+            public const Int32 MONITOR_DEFAULTTOPRIMERTY = 0x00000001;
+            public const Int32 MONITOR_DEFAULTTONEAREST = 0x00000002;
+
+
+            [DllImport("user32.dll")]
+            public static extern IntPtr MonitorFromWindow(IntPtr handle, Int32 flags);
+
+
+            [DllImport("user32.dll")]
+            public static extern Boolean GetMonitorInfo(IntPtr hMonitor, NativeMonitorInfo lpmi);
+
+
+            [Serializable, StructLayout(LayoutKind.Sequential)]
+            public struct NativeRectangle
+            {
+                public Int32 Left;
+                public Int32 Top;
+                public Int32 Right;
+                public Int32 Bottom;
+
+
+                public NativeRectangle(Int32 left, Int32 top, Int32 right, Int32 bottom)
+                {
+                    this.Left = left;
+                    this.Top = top;
+                    this.Right = right;
+                    this.Bottom = bottom;
+                }
+            }
+
+
+            [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+            public sealed class NativeMonitorInfo
+            {
+                public Int32 Size = Marshal.SizeOf(typeof(NativeMonitorInfo));
+                public NativeRectangle Monitor;
+                public NativeRectangle Work;
+                public Int32 Flags;
+            }
         }
 
         private void BtnCloseApplication_Click(object sender, RoutedEventArgs e)
